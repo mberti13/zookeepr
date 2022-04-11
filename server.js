64cd06eca40266c8877bf942999c5d70341cecea
MJB <matt.berti13@gmail.com>
2022-04-11 14:43:33 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 const animals = require('./data/animals.json');
 
 const express = require('express');
+const { type } = require('os');
 //instantiates the server
 const app = express();
 
 //Filters JSON files results by user parameters
 function filterByQuery(query, animalsArray){
 
-    let filteredResults = animalsArray;
+    //query for personality traits, personality traits are an array
+    let personalityTraits = [];
+
+    //save animalsArray as filteredResults here
+    //convert objects into array for .filter() method to work
+    let filteredResults = animalsArray.animals;
+
+    if(query.personalityTraits){
+
+        //save personalityTraits as dedicated array
+        //If personalityTraits is a string, place it into a new array and save
+        if(typeof query.personalityTraits === 'string'){
+           personalityTraitsArray = [query.personalityTraits]; 
+        }else{
+            personalityTraitsArray = query.personalityTraits;
+        }
+
+        //Loop through each trait in personalityTraits array:
+        personalityTraitsArray.forEach(trait => {
+            //Check the trait against each animal in the filteredResults array.
+            //Remember, it is initially a copy of the animalsArray,
+            //but here we're updating it for each trait in the .forEach() loop.
+            //For each trait being targeted in the filter, the filteredResults
+            //array will then contain only the entries that contain the trait,
+            //so at the end we'll have an array of animals that have every one
+            //of the traits when the .forEach() loop is finished
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
 
     if(query.diet){
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
@@ -18,13 +49,16 @@ function filterByQuery(query, animalsArray){
     if(query.name){
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
+
+    //return the filtered results
     return filteredResults;
 }
 
-
 //Creates the initial request/response into the API
 app.get('/api/animals', (req, res) =>{
+    
     let results = animals;
+    console.log(animals);
 
     // console.log(req.query)
     if(req.query){
